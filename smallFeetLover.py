d57d8490c6dbda4d12003fbed5e8cbfac9d644b1
Rishab Ramanathan <rrishab2001@gmail.com>
2020-04-09 14:39:33 +0530

    image reaction

--- smallFeetLover.py
+++ smallFeetLover.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='~')
+BOT = discord.Client()
 
 def getToken():
     tokenFile = open('/home/wondercoconut/python3/botshit/token.txt','r')
@@ -18,22 +19,26 @@ async def test(ctx):
 async def start(ctx):
     await ctx.channel.send('''
     press F to pay respects
-    ~getwoke for this message
-    ~nigga for cute poetry aka BARS
-    ~bruh for surprise
-    ~madarchod for haha very nice desi meme
-    say the word \"idiot\" for a strongly worded copypasta
-    ~bop for a bop
-    pigeon for something
-    be horny
+    #~getwoke for this message 
+    #~nigga for cute poetry aka BARS
+    #~bruh for surprise
+    #~madarchod for haha very nice desi meme
     ~engage to remove the big guns
     ~art for a personification of the internet
+    ~bop for a bop
+    ~formula for the greatest lego oc ever
+
+    dont be shy say hello
+
+    pigeon for something
+    say the word \"idiot\" for a strongly worded copypasta
+    be horny
     nice for a ceaser
     muda
     ask him what bear is best
     what what what what
     bible time ezekiel boy
-    ~formula for the greatest lego oc ever''')
+    ''')
 
 #commands list 
 
@@ -44,25 +49,46 @@ async def nigga(ctx):
 @bot.command(name = 'bruh')
 async def bruh(ctx):
     BRUH=open('/home/wondercoconut/python3/botshit/bruh.txt','r')
-    while(True):
-        try:
-            await ctx.channel.send(BRUH.read())
-        except:
-            break
-
+    BRUH=BRUH.read()
+    await ctx.channel.send(BRUH)
 
 @bot.command(name = 'madarchod')
 async def madarchod(ctx):
     await ctx.channel.send('mai madarchod hoon jo isme aaya')
 
-#@bot.event
-#async def on_message(ctx):
-#    if ctx.author == bot.user:
-#        return
-#    else:
-#        word=ctx.content
-#        word=word.split()
-        #await ctx.channel.send(word[0])
+@bot.event
+async def on_message(ctx):
+    if ctx.author == bot.user:
+        return
+    else:
+        word=ctx.content.lower()
+        if word.startswith('hello'):
+            await ctx.channel.send('fuck off')
+        else:
+              try:
+                  image=str(ctx.attachments[0])
+                  if isImage(image):
+                      await ctx.channel.send('img')
+                  #await ctx.channel.send(isImage(image))
+
+                  attachTest(Ctx.attachments)
+
+                  #print(ctx.attachments)                    
+              except:
+                    pass    
+        await bot.process_commands(ctx)
+            
+def isImage(image):
+    image_ext = ['jpg','png','jpeg']
+    x = False
+    for ext in image_ext:
+        if image.find(ext)!=-1:
+            x=True
+            break
+
+    return x
+
+
 #        i=0
 #        while(True):
 #            try:
