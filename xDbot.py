5bf0d82b8fba0e5c9f752e7b36bfc8100be65e65
Rishab Ramanathan <rrishab2001@gmail.com>
2020-04-13 14:00:19 +0530

    extra crap #1

--- xDbot.py
+++ xDbot.py
@@ -1,6 +1,11 @@
 import discord
 from discord.ext import commands
 
+from random import seed
+from random import randint
+
+import datetime
+
 bot = commands.Bot(command_prefix='$')
 BOT = discord.Client()
 
@@ -19,6 +24,22 @@ async def on_message(ctx):
     if ctx.author != bot.user:
         word =  ctx.content.lower()
         if word.find('xd') != -1:
-            await ctx.channel.send('i am alive')
+            await ctx.channel.send(xdRandom())
+
+def xdRandom():
+    time = datetime.datetime.now()
+    mic = time.microsecond
+    seed(mic)
+    i = randint(1,5)
+    if(i<=4):
+        xd =  open('/home/wondercoconut/python3/botshit/discord_bot/xD text/XD2.txt')
+        xd = xd.read()
+        xd = xd.split('\n')
+        return xd[i-1]
+    elif (i>=5):
+        xd =  open('/home/wondercoconut/python3/botshit/discord_bot/xD text/XD1.txt')
+        xd = xd.read()
+        return xd
 
 bot.run (getToken())
+
